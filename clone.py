58331dba5a38271f4c03c192e25ac005e9c2fc3a
Sarthak Sahu <sarthaksahu1988@gmail.com>
2017-04-10 13:20:13 +0000

    LeNet implementation

--- clone.py
+++ clone.py
@@ -24,13 +24,21 @@ y_train = np.array(measurements)
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Convolution2D(6, 28, 28, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Convolution2D(16, 10, 10, activation='relu'))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
