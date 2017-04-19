0efc7f8e8d3c4098d0a9341b92640afc1d74406c
Sarthak Sahu <sarthaksahu1988@gmail.com>
2017-04-19 14:00:23 +0000

    Add cropping layer
    - ignore top 50 px and bottom 20 px

--- clone.py
+++ clone.py
@@ -37,9 +37,11 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Convolution2D
 from keras.layers.pooling import MaxPooling2D
+from keras.layers import Cropping2D
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5))
 model.add(Convolution2D(6, 28, 28, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Convolution2D(16, 10, 10, activation='relu'))
